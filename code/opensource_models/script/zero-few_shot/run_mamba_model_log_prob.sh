7f507949a0ccbb2cf5252f2c635571a7af69cc75
syedarsalannaqvi <67072245+syedarsalannaqvi@users.noreply.github.com>
2025-03-13 09:08:01 -0700

    Add files via upload

---
+++ code/opensource_models/script/zero-few_shot/run_mamba_model_log_prob.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+export CUDA_VISIBLE_DEVICES=0
+
+
+export TRANSFORMERS_CACHE=/PATH/TO/CACHE/.cache
+
+
+python run_mamba_model_log_prob.py
---
+++ code/opensource_models/script/zero-few_shot/run_opensource_models_log_prob.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+export CUDA_VISIBLE_DEVICES=0
+
+
+export TRANSFORMERS_CACHE=/PATH/TO/CACHE/.cache
+
+python run_llama_mistral_model_log_prob.py
